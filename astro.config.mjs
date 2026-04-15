8a148b288dd63988aa1e1502c1bed38dad7efe61
hani <hani.lim@company.com>
2026-04-15 17:57:26 +0900

    빌드명령어로 테마 정하고 netlify로 배포

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from "astro/config";
+// import react from "@astrojs/react";
+// import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({
+//   integrations: [react(), tailwind()],
+//   output: "static", // S3 정적 배포를 위한 설정
+// });
+
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite"; // 이 부분이 @tailwindcss/vite 인지 확인
+
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: "static",
+});
